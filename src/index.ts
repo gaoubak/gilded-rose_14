90550fa166c99e4e33badf5a4f6125ea8c42dfc8
Juba Aitadda <aitaddajuba93@gmail.com>
2023-09-11 16:34:09 +0200

    Update Refactorer

--- src/index.ts
+++ src/index.ts
@@ -25,56 +25,44 @@ export class GildedRose {
     }
 
     private processItem(item: Item): void {
-        switch (item.name) {
-            case "Aged Brie":
-                this.updateAgedBrie(item);
-                break;
-            case "Backstage passes to a TAFKAL80ETC concert":
-                this.updateBackstagePass(item);
-                break;
-            case "Sulfuras, Hand of Ragnaros":
-                // Sulfuras ne change pas
-                break;
-            default:
-                this.updateNormalItem(item);
-                break;
-        }
-
-        // Réduire sellIn pour tous les articles sauf "Sulfuras"
         if (item.name !== "Sulfuras, Hand of Ragnaros") {
             item.sellIn -= 1;
-        }
 
-        // Traitement après la date de vente
-        if (item.sellIn < 0) {
-            this.handleExpired(item);
+            switch (item.name) {
+                case "Aged Brie":
+                    this.updateAgedBrie(item);
+                    break;
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    this.updateBackstagePass(item);
+                    break;
+                default:
+                    this.updateNormalItem(item);
+                    break;
+            }
+
+            if (item.sellIn < 0) {
+                this.handleExpired(item);
+            }
         }
     }
 
+
     private updateAgedBrie(item: Item): void {
-        if (item.quality < 50) {
-            item.quality += 1;
-        }
+        item.quality = item.quality < 50 ? item.quality + 1 : item.quality;
     }
 
     private updateBackstagePass(item: Item): void {
-        if (item.quality < 50) {
-            item.quality += 1;
-
-            if (item.sellIn < 11) {
-                item.quality += 1;
-            }
-
-            if (item.sellIn < 6) {
-                item.quality += 1;
-            }
+        if (item.sellIn <= 0) {
+            item.quality = 0;
+            return;
         }
+
+        item.quality += item.sellIn < 6 ? 3 : item.sellIn < 11 ? 2 : 1;
+        item.quality = Math.min(item.quality, 50);
     }
 
     private updateNormalItem(item: Item): void {
-        if (item.quality > 0) {
-            item.quality -= 1;
-        }
+        item.quality = item.quality > 0 ? item.quality - 1 : item.quality;
     }
 
     private handleExpired(item: Item): void {
@@ -82,7 +70,7 @@ export class GildedRose {
             this.updateAgedBrie(item);
         } else if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
             item.quality = 0;
-        } else if (item.quality > 0) {
+        } else {
             this.updateNormalItem(item);
         }
     }
