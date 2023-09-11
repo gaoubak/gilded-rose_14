8e6e818bcfbd15e2755ac111ccab4d6378aa82b2
Juba Aitadda <aitaddajuba93@gmail.com>
2023-09-11 17:02:55 +0200

    Optimisation Refactorer

--- src/index.ts
+++ src/index.ts
@@ -18,60 +18,49 @@ export class GildedRose {
     }
 
     updateQuality(): Array<Item> {
-        for (const item of this.items) {
-            this.processItem(item);
-        }
+        this.items.forEach(item => {
+            if (item.name !== "Sulfuras, Hand of Ragnaros") {
+                this.adjustSellIn(item);
+                this.adjustQuality(item);
+            }
+        });
         return this.items;
     }
 
-    private processItem(item: Item): void {
-        if (item.name !== "Sulfuras, Hand of Ragnaros") {
-            item.sellIn -= 1;
-
-            switch (item.name) {
-                case "Aged Brie":
-                    this.updateAgedBrie(item);
-                    break;
-                case "Backstage passes to a TAFKAL80ETC concert":
-                    this.updateBackstagePass(item);
-                    break;
-                default:
-                    this.updateNormalItem(item);
-                    break;
-            }
-
-            if (item.sellIn < 0) {
-                this.handleExpired(item);
-            }
-        }
+    private adjustSellIn(item: Item): void {
+        item.sellIn -= 1;
     }
 
-
-    private updateAgedBrie(item: Item): void {
-        item.quality = item.quality < 50 ? item.quality + 1 : item.quality;
+    private adjustQuality(item: Item): void {
+        switch (item.name) {
+            case "Aged Brie":
+                this.adjustAgedBrieQuality(item);
+                break;
+            case "Backstage passes to a TAFKAL80ETC concert":
+                this.adjustBackstagePassQuality(item);
+                break;
+            default:
+                this.adjustDefaultQuality(item);
+                break;
+        }
+        this.ensureQualityBounds(item);
     }
 
-    private updateBackstagePass(item: Item): void {
-        if (item.sellIn <= 0) {
-            item.quality = 0;
-            return;
-        }
+    private adjustAgedBrieQuality(item: Item): void {
+        item.quality += 1;
+        if (item.sellIn < 0) item.quality += 1;
+    }
 
+    private adjustBackstagePassQuality(item: Item): void {
         item.quality += item.sellIn < 6 ? 3 : item.sellIn < 11 ? 2 : 1;
-        item.quality = Math.min(item.quality, 50);
+        if (item.sellIn < 0) item.quality = 0;
     }
 
-    private updateNormalItem(item: Item): void {
-        item.quality = item.quality > 0 ? item.quality - 1 : item.quality;
+    private adjustDefaultQuality(item: Item): void {
+        item.quality -= item.sellIn < 0 ? 2 : 1;
     }
 
-    private handleExpired(item: Item): void {
-        if (item.name === "Aged Brie") {
-            this.updateAgedBrie(item);
-        } else if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
-            item.quality = 0;
-        } else {
-            this.updateNormalItem(item);
-        }
+    private ensureQualityBounds(item: Item): void {
+        item.quality = Math.max(0, Math.min(item.quality, 50));
     }
 }
