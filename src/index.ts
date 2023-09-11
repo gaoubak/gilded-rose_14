fd16bdd52033c12e6ca0580300e16c9c6ce23e74
Juba Aitadda <aitaddajuba93@gmail.com>
2023-09-11 16:13:40 +0200

    Refactorer

--- src/index.ts
+++ src/index.ts
@@ -8,61 +8,82 @@ export class Item {
         this.sellIn = sellIn;
         this.quality = quality;
     }
-
-    updateQuality(): void { }
-    updateSellIn(): void {
-        this.sellIn -= 1;
-    }
-}
-
-export class AgedBrie extends Item {
-    updateQuality(): void {
-        if (this.quality < 50) {
-            this.quality += 1;
-        }
-    }
-}
-
-export class BackstagePasses extends Item {
-    updateQuality(): void {
-        if (this.sellIn <= 0) {
-            this.quality = 0;
-        } else if (this.sellIn <= 5) {
-            this.quality = Math.min(50, this.quality + 3);
-        } else if (this.sellIn <= 10) {
-            this.quality = Math.min(50, this.quality + 2);
-        } else if (this.quality < 50) {
-            this.quality += 1;
-        }
-    }
-}
-
-export class Sulfuras extends Item {
-    updateQuality(): void { }
-    updateSellIn(): void { }
-}
-
-export class NormalItem extends Item {
-    updateQuality(): void {
-        if (this.sellIn <= 0) {
-            this.quality = Math.max(0, this.quality - 2);
-        } else {
-            this.quality = Math.max(0, this.quality - 1);
-        }
-    }
 }
 
 export class GildedRose {
     items: Array<Item>;
 
-    constructor(items: Array<Item>) {
+    constructor(items: Array<Item> = []) {
         this.items = items;
     }
 
-    updateItems(): void {
+    updateQuality(): Array<Item> {
         for (const item of this.items) {
-            item.updateQuality();
-            item.updateSellIn();
+            this.processItem(item);
+        }
+        return this.items;
+    }
+
+    private processItem(item: Item): void {
+        switch (item.name) {
+            case "Aged Brie":
+                this.updateAgedBrie(item);
+                break;
+            case "Backstage passes to a TAFKAL80ETC concert":
+                this.updateBackstagePass(item);
+                break;
+            case "Sulfuras, Hand of Ragnaros":
+                // Sulfuras ne change pas
+                break;
+            default:
+                this.updateNormalItem(item);
+                break;
+        }
+
+        // Réduire sellIn pour tous les articles sauf "Sulfuras"
+        if (item.name !== "Sulfuras, Hand of Ragnaros") {
+            item.sellIn -= 1;
+        }
+
+        // Traitement après la date de vente
+        if (item.sellIn < 0) {
+            this.handleExpired(item);
+        }
+    }
+
+    private updateAgedBrie(item: Item): void {
+        if (item.quality < 50) {
+            item.quality += 1;
+        }
+    }
+
+    private updateBackstagePass(item: Item): void {
+        if (item.quality < 50) {
+            item.quality += 1;
+
+            if (item.sellIn < 11) {
+                item.quality += 1;
+            }
+
+            if (item.sellIn < 6) {
+                item.quality += 1;
+            }
+        }
+    }
+
+    private updateNormalItem(item: Item): void {
+        if (item.quality > 0) {
+            item.quality -= 1;
+        }
+    }
+
+    private handleExpired(item: Item): void {
+        if (item.name === "Aged Brie") {
+            this.updateAgedBrie(item);
+        } else if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
+            item.quality = 0;
+        } else if (item.quality > 0) {
+            this.updateNormalItem(item);
         }
     }
 }
